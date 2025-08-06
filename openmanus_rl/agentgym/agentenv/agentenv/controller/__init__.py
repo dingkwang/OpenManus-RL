f436d98685c68bcbe2bed8a155d8be7e455de5c3
Murphy Tian <murphytian06@gmail.com>
2025-08-06 15:46:55 +0800

    chore: Remove agentgym env support

--- openmanus_rl/agentgym/agentenv/agentenv/controller/__init__.py
+++
@@ -1,5 +0,0 @@
-from .agent import Agent
-from .env import BaseEnvClient, StepOutput
-# from .task import BaseTask, ConversationMessage, TokenizedConversationOutput
-from .task import BaseTask, ConversationMessage
-from .utils import Evaluator
